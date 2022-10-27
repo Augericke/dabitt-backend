c5a8905736df6fb1dd6b7d22527473b60b60f26c
augericke <augericke@gmail.com>
2022-10-27 15:22:46 -0400

    add default port

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 import config from "./utils/config";
 
-app.listen(config.server.port, () => {
+app.listen(config.server.port || 3001, () => {
   console.log(`listening on port: ${config.server.port || 3001}`);
 });
